8d9de7c09e0d7d613018fea7590df7e741278501
lkvreddy007 <lkvreddy007@gmail.com>
2020-10-14 21:57:21 +0530

    [Krishna]added-RemoveAvoidableWordFromThePhaseAndReturnNull

--- test/java/com/capg/MyLinkedHashMapTest.java
+++ test/java/com/capg/MyLinkedHashMapTest.java
@@ -30,5 +30,25 @@ public class MyLinkedHashMapTest {
 		Assert.assertEquals(3, frequency);
 	}
 
-
+	@Test
+	public void givenASentence_WhenWordIsDeleted_ShouldReturnNull() {
+		String sentence="Paranoids are not paranoid because"
+				+ "they are paranoid but because they keep putting"
+				+ "themselves deliberately into paranoid avoidable situations";
+		LinkedHashMap<String,Integer> linkedHashMap=new LinkedHashMap<>();
+		String[] words=sentence.toLowerCase().split(" ");
+		for(String word: words) {
+			Integer value= linkedHashMap.get(word);
+			if(value==null) {
+				value=1;
+			}
+			else {
+				value=value+1;
+			}
+			linkedHashMap.add(word,value);
+		}
+		linkedHashMap.remove("avoidable");
+		System.out.println(linkedHashMap);
+		Assert.assertNull(linkedHashMap.get("avoidable"));
+	}
 }
